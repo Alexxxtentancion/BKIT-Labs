5f091f4c28fc61c6591d085df1f5106c33706215
Alexxxtentancion <32328072+Alexxxtentancion@users.noreply.github.com>
2017-12-06 12:10:31 +0300

    Add files via upload

---
+++ lab4/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace Лаба_Формы
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 57);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Чтение файла";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 161);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 62);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Поиск слова";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(187, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Колличество слов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Время загрузки:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(187, 161);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ключевое слово:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(187, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(104, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Время поиска:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(338, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(113, 22);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(338, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(113, 22);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(338, 161);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(113, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(338, 203);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(113, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(612, 84);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(302, 351);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(207, 53);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Закрыть окно";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(787, 428);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Работа с файловой системой";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ lab4/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаба_Формы
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<string> list = new List<string>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog FileRead = new OpenFileDialog();
+            FileRead.Filter = "текстовые файлы |*.txt";
+            if (FileRead.ShowDialog() == DialogResult.OK)
+            {
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                string text = File.ReadAllText(FileRead.FileName);
+                char[] separators = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n' };
+                string[] textArray = text.Split(separators);
+                foreach (string strTemp in textArray)
+                {
+                    string str = strTemp.Trim();
+                    if (!list.Contains(str)) list.Add(str);
+
+                }
+                timer.Stop();
+
+                this.textBox2.Text = timer.Elapsed.ToString();
+                this.textBox1.Text = list.Count.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Необхоимо выбрать файл");
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+       
+        {
+            string word = this.textBox3.Text.Trim();
+            if(!string.IsNullOrWhiteSpace(word) && list.Count>0)
+            {
+                string wordUpper = word.ToUpper();
+                List<string> tempList = new List<string>();
+                Stopwatch timer = new Stopwatch();
+                timer.Start();
+                foreach(string str in list)
+                {
+                    if(str.ToUpper().Contains(wordUpper))
+                    {
+                        tempList.Add(str);
+                    }
+                     
+                }
+                timer.Stop();
+                this.textBox4.Text = timer.Elapsed.ToString();
+                this.listBox1.BeginUpdate();
+                this.listBox1.Items.Clear();
+                foreach(string str in tempList)
+                {
+                    this.listBox1.Items.Add(str);
+                }
+                this.listBox1.EndUpdate();
+            }
+            else
+            {
+                MessageBox.Show("Выберите файл и ввеите ключевое слово!");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вы действительно хотите закрыть форрму?","Уважаемый пользователь,",MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if(result ==DialogResult.No )
+            {
+                e.Cancel = true;
+            }
+
+        }
+    }
+}
